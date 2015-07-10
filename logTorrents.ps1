4b14ecef1bbf3735087d0d3ccc7a7d3cfacee01a
cassdotcom <cassdotcom@gmail.com>
2015-07-10 15:02:45 +0100

    first

---
+++ logTorrents.ps1
@@ -0,0 +1,33 @@
+﻿
+Param(
+    [Parameter(Mandatory=$true)]
+    [System.String]
+    $torrentPath,
+    [Parameter(Mandatory=$true)]
+    [System.String]
+    $torrentName
+)
+
+
+
+
+
+    Try
+    {
+        $logFile = "C:\Users\cass\Downloads\torr_logs.txt"
+$msg = @"
+$(Get-Date -Format "yyyy-MM-dd`tHH-mm-ss)
+$(Get-Date -Format "yyyy-MM-dd`tHH-mm-ss)`t$($torrentName)
+$(Get-Date -Format "yyyy-MM-dd`tHH-mm-ss)`t$($torrentPath)
+"@
+
+        $msg | Out-File $logFile -Append
+
+    } Catch {
+        
+        break
+
+    }
+
+
+
